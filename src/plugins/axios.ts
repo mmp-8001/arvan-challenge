6166d0f651f0169235cb3c8482bd5735a717ffd6
Mohammad Matin Parvin <59414338+mmp-8001@users.noreply.github.com>
2024-10-26 20:25:53 +0330

    feat(api): create instance of axios

--- src/plugins/axios.ts
+++ src/plugins/axios.ts
@@ -1,9 +1,28 @@
 import axios from 'axios'
 
 // init axios
-const instance = axios.create({
+export const instance = axios.create({
   timeout: 10000,
   baseURL: import.meta.env.VITE_BASE_URL,
 })
+
+instance.interceptors.request.use(
+  config => {
+    // retrieve jwt-token if exist
+    const token = localStorage.getItem('jwt-token')
+
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`
+    }
+
+    return config
+  },
+  error => Promise.reject(error),
+)
+
+instance.interceptors.response.use(
+  ({ data }) => data,
+  ({ message, response }) => Promise.reject(response ? response.data : message),
+)
 
 export default instance
