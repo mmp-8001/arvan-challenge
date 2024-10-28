e02fbdd9354691e372d0ecad12798d2f94b0ec8f
Mohammad Matin Parvin <59414338+mmp-8001@users.noreply.github.com>
2024-10-28 12:33:36 +0330

    test(input): add attribute label to input element

--- src/components/plugins/CInput.vue
+++ src/components/plugins/CInput.vue
@@ -53,6 +53,7 @@ const errorObject = computed(() => ({
 <template>
   <div class="mb-5 w-full">
     <label
+      data-test-id="label"
       :for="name"
       class="block mb-2 text-sm font-medium text-body-1"
       :class="{ '!text-error': errorObject.hasError }"
@@ -61,6 +62,7 @@ const errorObject = computed(() => ({
     <input
       :id="name"
       v-model="model"
+      data-test-id="input"
       type="text"
       class="outline-none border border-gray-300 text-gray-900 text-sm rounded-md focus:border-blue-500 block w-full p-2.5"
       :class="[errorObject.errorInputClass]"
@@ -69,7 +71,7 @@ const errorObject = computed(() => ({
     />
 
     <div class="pt-1 text-sm text-error">
-      <span v-show="errorObject.hasError"
+      <span v-show="errorObject.hasError" data-test-id="error"
         >{{ errorObject.errorText }} &nbsp;</span
       >
     </div>
