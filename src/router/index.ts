67ab619941a845b129fe15b2482ac1d06e132b80
Mohammad Matin Parvin <59414338+mmp-8001@users.noreply.github.com>
2024-10-28 17:52:00 +0330

    fix(route): fix login and register route

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import LoginView from '../views/auth/LoginView.vue'
+import AuthLayout from '@/layouts/AuthLayout.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -11,9 +12,23 @@ const router = createRouter({
       component: HomeView,
     },
     {
-      path: '/login',
-      name: 'login',
-      component: LoginView,
+      path: '/',
+      component: AuthLayout,
+      children: [
+        {
+          path: 'register',
+          name: 'Register',
+          // route level code-splitting
+          component: () => import('../views/auth/RegisterView.vue'),
+          alias: '/register',
+        },
+        {
+          path: 'login',
+          name: 'Login',
+          component: LoginView,
+          alias: '/login',
+        },
+      ],
     },
   ],
 })
---
+++ src/views/auth/RegisterView.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="card">
+    <h1 class="text-3xl">register view</h1>
+  </div>
+</template>
