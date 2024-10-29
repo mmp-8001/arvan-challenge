0cc313732dd2cb801dffde540f581dd9c2cf580f
Mohammad Matin Parvin <59414338+mmp-8001@users.noreply.github.com>
2024-10-29 10:13:40 +0330

    feat(component): add buttoon and loader

--- src/components/plugins/CButton.vue
+++ src/components/plugins/CButton.vue
@@ -1,8 +1,19 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import CLoader from '@/components/plugins/CLoader.vue'
+
+// define props
+const { loading = false } = defineProps<{
+  loading?: boolean
+}>()
+</script>
 
 <template>
-  <button class="block bg-primary w-full rounded p-2 text-white">
-    <slot />
+  <button
+    :disabled="loading"
+    class="block bg-primary relative overflow-hidden w-full rounded p-2 text-white"
+  >
+    <c-loader v-show="loading" />
+    <span :class="{ '!invisible': loading }"><slot /></span>
   </button>
 </template>
 
