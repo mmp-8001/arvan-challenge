b87f55baf23c2eec47e8fb4419f6858bc244b166
Mohammad Matin Parvin <59414338+mmp-8001@users.noreply.github.com>
2024-10-31 01:52:30 +0330

    feat(plugin): add remain error handling to api error handler

--- src/plugins/api-error.ts
+++ src/plugins/api-error.ts
@@ -1,8 +1,10 @@
 import type { ApiErrorResponse } from '@/types'
 import { useToast } from 'vue-toastification'
 import CToast from '@/components/plugins/CToast.vue'
+import { useUserStore } from '@/stores/user'
+import router from '@/router'
 
-export function handleErrorResponse(
+export async function handleErrorResponse(
   error: {
     status: number
     data: ApiErrorResponse
@@ -18,6 +20,30 @@ export function handleErrorResponse(
           component: CToast,
           props: { additional: data.message, title: title },
         })
+        break
+      // Unauthenticated
+      case 401:
+        useToast().error({
+          component: CToast,
+          props: { additional: data.message, title: title },
+        })
+        await useUserStore().logout()
+        break
+      // unauthorized action
+      case 403:
+        useToast().error({
+          component: CToast,
+          props: { additional: data.message, title: 'Not Allowed! ' },
+        })
+
+        break
+      // not found resource
+      case 404:
+        useToast().error({
+          component: CToast,
+          props: { additional: data.message, title: 'Not Found! ' },
+        })
+        await router.push({ name: 'Error' })
         break
       default:
         useToast().error({ component: CToast, props: { textType: 'network' } })
