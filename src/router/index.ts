d8fafbbabdf274eb513c4cf64b21292d4d273518
Mohammad Matin Parvin <59414338+mmp-8001@users.noreply.github.com>
2024-10-31 01:51:15 +0330

    feat(route): add articles and error routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,16 +1,50 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+
 import LoginView from '../views/auth/LoginView.vue'
 import AuthLayout from '@/layouts/AuthLayout.vue'
 import { authMiddleware } from '@/middleware/authRouter'
+import DefaultLayout from '@/layouts/DefaultLayout.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
+  strict: true,
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView,
+      component: DefaultLayout,
+      children: [
+        {
+          name: 'Home',
+          path: '/',
+          redirect: '/articles',
+        },
+        {
+          name: 'ArticlesEdit',
+          path: '/articles/edit/:slug',
+          // route level code-splitting
+          component: () => import('../views/EditArticleView.vue'),
+        },
+        {
+          name: 'ArticlesCreate',
+          path: '/articles/create',
+          // route level code-splitting
+          component: () => import('../views/CreateArticleView.vue'),
+        },
+        {
+          alias: '/articles',
+          path: '/articles/page/:page(\\d+)?',
+          // route level code-splitting
+          component: () => import('../views/ArticlesView.vue'),
+          name: 'Articles',
+          props: route => ({ page: parseInt(<never>route.params.page) || 1 }),
+        },
+        {
+          name: 'Error',
+          path: '/404',
+          // route level code-splitting
+          component: () => import('../views/ErrorView.vue'),
+        },
+      ],
     },
     {
       path: '/',
@@ -31,6 +65,7 @@ const router = createRouter({
         },
       ],
     },
+    { path: '/:pathMatch(.*)*', redirect: { name: 'Error' } },
   ],
 })
 
